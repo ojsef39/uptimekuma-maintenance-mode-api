e643a48f4c1774a7278e6b8d02456ed22113d5f6
Josef Hofer <jhofer@united-internet.de>
2023-06-19 15:00:56 +0000

    v2.1

---
+++ uptime-api.py
@@ -0,0 +1,78 @@
+import argparse
+import logging
+import sys
+import os
+from uptime_kuma_api import UptimeKumaApi
+
+def init():
+    global api
+
+    parser = argparse.ArgumentParser(prog='Uptime-API-Maintenance-mode',
+                                     description='Python script to control '
+                                                 'maintenance modes in uptime '
+                                                 'kuma depending on Proxmox'
+                                                 ' Backups')
+    parser.add_argument('--log',
+                        default='WARNING',
+                        help="Set LogLevel DEBUG, INFO, WARNING, ERROR,"
+                             " CRITICAL; If not set manually LogLevel "
+                             "is set to WARNING by default")
+
+    parser.add_argument('--host',
+                        default=None,
+                        help="Set host for which maintenance mode should be"
+                             "activated/deactivated")
+
+    args = parser.parse_args().__dict__
+    log_level = args["log"].upper()
+    mm_host = args["host"]
+
+# Check if log_level is set
+    if str(log_level) == "NOTHING" or log_level is None:
+        log_level = "INFO"
+    # Check if log_level is valid
+    elif log_level not in ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]:
+        logging.critical(
+            "LogLevel: " + str(log_level) +
+            ' is not valid! Please enter NOTHING or a VALID option!'
+            ' See example.env or docs!')
+        sys.exit()
+
+    # Set logging
+    loglevel_err = getattr(logging, log_level)
+    # Check if logging is accepting set log_level
+    if not isinstance(loglevel_err, int):
+        raise ValueError('Invalid log level: %s' % log_level)
+    logging.basicConfig(level=log_level,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    # Display set log_level
+    logging.info("LogLevel is set to: " + log_level)
+
+    # Check if Host is set
+    if mm_host == None:
+        logging.critical("No maintenance mode host set… exiting")
+        sys.exit()
+
+    # Login to Uptime Kuma
+    try:
+        api = UptimeKumaApi(os.getenv("UPTIME_URL"))
+        api.login(os.getenv('UPTIME_USER'), os.getenv("UPTIME_PASS"))
+        # Login by token is not working for some reason?
+        #api.login_by_token(os.getenv("UPTIME_TOKEN"))
+        logging.debug("API INFO: " + str(api.info()))
+    except:
+        logging.error("There was an error while trying to login. Please try "
+                      "again.")
+        if log_level == "DEBUG":
+            raise
+
+def get_monitor():
+    print(api.get_monitors())
+
+def main():
+    init()
+    get_monitor()  # test function
+
+
+if __name__ == '__main__':
+    main()
